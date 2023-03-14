bcb8ab30360ee4cdf5678b9dc5a7479c39ddb116
naxinga <ngarconf@hotmail.com>
2023-03-14 11:55:13 +0000

    Funciones hechas

--- my-app/src/Problema1.js
+++ my-app/src/Problema1.js
@@ -3,71 +3,49 @@ import React from 'react';
 export const P1 = () => {
 
     function calculateIntervalDays(fromDate, toDate = '10/03/2023') {
-        var dateOne = new Date (fromDate);
-        var dateTwo = new Date (toDate);
+        const date = new Date(fromDate);
+        const toDateObj = new Date(toDate);
+        const timeDiff = toDateObj.getTime() - date.getTime();
+        return Math.ceil(timeDiff / (1000 * 3600 * 24));
+      }
+      
+      function removeVocalsAndSpaces(name) {
+        return name.replace(/[aeiouáéíóúü\s]/gi, "");
+      }
+      
+      function handleInputName() {
+        const nameInput = document.getElementById("name");
+        const name = nameInput.value;
+        const nameWithoutVocalsAndSpaces = removeVocalsAndSpaces(name);
+        nameInput.value = nameWithoutVocalsAndSpaces;
+      }
+      
+      function handleInputDate() {
+        const dateInput = document.getElementById("date");
+        const dateStr = dateInput.value;
+        const daysInput = document.getElementById("days");
+        const days = calculateIntervalDays(dateStr);
+        daysInput.value = days;
+      }
 
-        var days = dateOne-dateTwo;
-        
-        return (
-            days
-        )
-    }
-    
-    function removeVocalsAndSpaces(name) {
-        let lowName = name.toLowerCase();
-        return lowName.replace(/[aeiou]/ig,'');
-    }
-    
-    function handleInputName() {
-    }
-    
-    function handleInputDate() {
-    }
 
-    
 
-    return(
-        <>
-
-        <p>Probando</p>
-
-        
-        <p>
-            {console.log(removeVocalsAndSpaces("Hola"))}
-        </p>
-        </>
+    return (
+        <form>
+            <label for="name">Nombre:</label>
+            <input type="text" id="name" onblur="handleInputName()" />
+            <br />
+            <label for="nameWithoutVocalsAndSpaces">Nombre sin vocales:</label>
+            <input type="text" id="nameWithoutVocalsAndSpaces" disabled />
+            <br />
+            <label for="date">Fecha:</label>
+            <input type="text" id="date" onblur="handleInputDate()" />
+            <br />
+            <label for="days">Días transcurridos hasta el 10/03/2023:</label>
+            <input type="text" id="days" disabled />
+            {console.log(calculateIntervalDays("16/06/2023",'10/03/2023'))}
+        </form>
     )
 }
 
 
-/*Hoy = new Date();//Fecha actual del sistema
-
-var AnyoFecha = Fecha1.getFullYear();
-var MesFecha = Fecha1.getMonth();
-var DiaFecha = Fecha1.getDate();
-
-var AnyoHoy = Hoy.getFullYear();
-var MesHoy = Hoy.getMonth();
-var DiaHoy = Hoy.getDate();
-
-if (AnyoFecha < AnyoHoy){
-    alert ("La fecha introducida es anterior a Hoy");
-}
-else{
-    if (AnyoFecha == AnyoHoy && MesFecha < MesHoy){
-        alert ("La fecha introducida es anterior a Hoy");			
-    }
-    else{
-        if (AnyoFecha == AnyoHoy && MesFecha == MesHoy && DiaFecha < DiaHoy){
-            alert ("La fecha introducida es anterior a Hoy");
-        }
-        else{
-            if (AnyoFecha == AnyoHoy && MesFecha == MesHoy && DiaFecha == DiaHoy){
-                 alert ("Has introducido la fecha de Hoy");
-            }
-            else{
-                alert ("La fecha introducida es posterior a Hoy");
-            }
-        }
-    }
-}*/
